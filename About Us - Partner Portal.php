1851839faad885d2994f2fb87efcdb8cb380e411
Asronix <101865611+Asronix@users.noreply.github.com>
2022-04-06 14:51:34 +0800

    jerry

--- About Us - Partner Portal.php
+++ About Us - Partner Portal.php
@@ -23,105 +23,107 @@
         </div>
             
 <!--Partnerships -->
-        <div class="col-sm-12 container-fluid d-flex m-0 p-0 partnershipsBD">
 
-                <div class="row-sm-9 m-0 members_seperatorbg1">
-                    <div class= "position-absolute top-75 start-50 translate-middle px-5 py-2 pPortalTitleBg"> 
-                    <h3 class="text-center fw-bold"> Partnerships </h3>  
-                    </div>
+<div class="col-sm-12 container-fluid d-flex m-0 p-0 mb-5 partnershipsBD">
+
+    <div class="row-sm-9 m-0 members_seperatorbg1">
+        <div class= "position-absolute top-75 start-50 translate-middle px-5 py-2 pPortalTitleBg"> 
+        <h3 class="text-center fw-bold"> Partnerships </h3>  
+        </div>
+    </div>
+
+    <div class="col-sm-10.5 pt-5 d-flex justify-content-center partnershipsContainer1">
+    <div class="container-fluid m-0 p-0 partnershipsContainer2">
+
+        <div class="container-fluid m-0 pb-5 d-flex justify-content-center partner_disp">
+                <div class="col-sm-6 align-self-center">  
+                    <img class="image-fluid d-flex mx-auto justify-content-center image_p" alt="world" src="assets\about us\woldforworld.png" >                       
+                </div>     
+                <div class="col-sm-5 justify-content-center description_1">
+                    <span class="text-center fs-4 partnerDescription"> 
+                        Joining the world for world organization (UN NGO IRENE Network),  to support and to act for the achievements of 
+                        the Global Agenda. WFWO believe that only with global partnerships and solidarity of all, and hearing the voices 
+                        and views of those most affected poorest and most most marginalized people, they will not miss their target. Everyone’s 
+                        involvement can make a difference. </span>
+                </div>               
+        </div>
+
+        <div class="container-fluid m-0 pb-5 d-flex justify-content-center partner_disp">
+                <div class="col-sm-6 align-self-center">  
+                    <img src="assets\about us\undesa.png" class="image-fluid d-flex mx-auto justify-content-center image_p">                       
+                </div>     
+                <div class="col-sm-5 justify-content-center mt-5">
+                    <span class="text-center fs-4 partnerDescription"> 
+                    Partners for Sustainable Development  of UN-DESA <br>
+                    #33126 Ref #188223 </span>
+                </div>          
+        </div>
+
+        <div class="container-fluid m-0 pb-5 d-flex justify-content-center partner_disp">
+                <div class="col-sm-6 align-self-center">  
+                    <img src="assets\about us\UIA.png" class="image-fluid d-flex mx-auto justify-content-center image_1">                       
+                </div>     
+                <div class="col-sm-5 justify-content-center mt-5">
+                    <span class="text-center fs-4 partnerDescription"> 
+                    Union of Internation Relations </span>
+                </div>               
+        </div>
+
+        <div class="container-fluid m-0 pb-5 d-flex justify-content-center partner_disp">
+                <div class="col-sm-6 align-self-center">  
+                    <img src="assets\about us\ec_logo.png" class="image-fluid d-flex mx-auto justify-content-center image_p">                       
+                </div>     
+                <div class="col-sm-5 justify-content-center mt-5 ">
+                    <span class="text-center fs-4 partnerDescription"> 
+                    United Nations Global Marketplace <br> #635673</span>
+                </div>              
+        </div>
+
+        <div class="container-fluid m-0 pb-5 d-flex justify-content-center partner_disp">
+                <div class="col-sm-6 align-self-center">  
+                    <img src="assets\about us\UPP.jpg" class="image-fluid d-flex mx-auto justify-content-center image_p">                       
+                </div>     
+                <div class="col-sm-5 justify-content-center mt-5">
+                    <span class="text-center fs-4 partnerDescription"> 
+                    UN Partner Portal</span>
                 </div>
+        </div>
 
-            <div class="col-sm-10.5 pt-5 d-flex justify-content-center partnershipsContainer1">
-                <div class="container-fluid m-0 p-0 partnershipsContainer2">
+        <div class="container-fluid m-0 pb-5 d-flex justify-content-center partner_disp">
+                <div class="col-sm-6 align-self-center">  
+                    <img src="assets\about us\VolunteerMatch.png" class="image-fluid d-flex mx-auto justify-content-center image_p">                       
+                </div>     
+                <div class="col-sm-5 mt-5 justify-content-center">
+                    <span class="text-center fs-4 partnerDescription"> 
+                    Volunteer Match</span>
+                </div>
+        </div>
 
-                    <div class="container-fluid m-0 p-1 d-flex justify-content-center partner_disp">
-                            <div class="col-sm-4">  
-                                <img class="image-fluid justify-content-center w-75" alt="world" src="assets\about us\woldforworld.png" >                       
-                            </div>     
-                            <div class="col-sm-3 p-3 justify-content-center description_1">
-                                <span class="text-center partnerDescription"> 
-                                    Joining the world for world organization (UN NGO IRENE Network),  to support and to act for the achievements of 
-                                    the Global Agenda. WFWO believe that only with global partnerships and solidarity of all, and hearing the voices 
-                                    and views of those most affected poorest and most most marginalized people, they will not miss their target. Everyone’s 
-                                    involvement can make a difference. </span>
-                            </div>               
-                    </div>
+        <div class="row-sm-6 mt-5 mb-4 ml-5 mr-5 pPortal_seperatorbg1">
+            <div class= "col-sm-6 position-relative start-50 translate-middle mt-5 py-2 pPortalTitleBg"> 
+            <h3 class="text-center fw-bold"> United Nation World Peace Association Japan also 
+                Donates to the following every month. </h3>  
+            </div>
+        </div>
 
-                    <div class="container-fluid m-0 pt-5 pb-5 d-flex justify-content-center partner_disp">
-                            <div class="col-sm-4 justify-content-center partnerImg">  
-                                <img src="assets\about us\undesa.png" class="img-fluid partnerImage">                       
-                            </div>     
-                            <div class="col-sm-3 p-3 justify-content-center">
-                                <span class="text-center partnerDescription"> 
-                                Partners for Sustainable Development  of UN-DESA <br>
-                                #33126 Ref #188223 </span>
-                            </div>          
-                    </div>
-
-                    <div class="container-fluid m-0 pt-5 pb-5 d-flex justify-content-center partner_disp">
-                            <div class="col-sm-4 justify-content-center partnerImg">  
-                                <img src="assets\about us\UIA.png" class="img-fluid-partnerImage">                       
-                            </div>     
-                            <div class="col-sm-3 p-5 justify-content-center">
-                                <span class="text-center partnerDescription"> 
-                                Union of Internation Relations </span>
-                            </div>               
-                    </div>
-
-                    <div class="container-fluid m-0 pt-5 pb-5 d-flex justify-content-center partner_disp">
-                            <div class="col-sm-4 justify-content-center partnerImg">  
-                                <img src="assets\about us\ec_logo.png" class="img-fluid-partnerImage">                       
-                            </div>     
-                            <div class="col-sm-3 p-5 justify-content-center">
-                                <span class="text-center partnerDescription"> 
-                                United Nations Global Marketplace <br> #635673</span>
-                            </div>              
-                    </div>
-
-                    <div class="container-fluid m-0 pt-5 pb-5 d-flex justify-content-center partner_disp">
-                            <div class="col-sm-4 justify-content-center partnerImg">  
-                                <img src="assets\about us\UPP.jpg" class="img-fluid-partnerImage">                       
-                            </div>     
-                            <div class="col-sm-3 justify-content-center">
-                                <span class="text-center partnerDescription"> 
-                                UN Partner Portal</span>
-                            </div>               
-                    </div>
-
-                    <div class="container-fluid m-0 pt-5 pb-5-5 d-flex justify-content-center partner_disp">
-                            <div class="col-sm-4 justify-content-center partnerImg">  
-                                <img src="assets\about us\VolunteerMatch.png" class="img-fluid-partnerImage">                       
-                            </div>     
-                            <div class="col-sm-3 p-3 justify-content-center">
-                                <span class="text-center partnerDescription"> 
-                                Volunteer Match</span>
-                            </div>               
-                    </div>
-
-                    <div class="row-sm-9 m-0 mt-2 mb-2 members_seperatorbg1">
-                        <div class= "position-relative top-75 start-50 translate-middle mt-5 px-5 py-2 pPortalTitleBg"> 
-                        <h3 class="text-center fw-bold"> United Nation World Peace Association Japan also 
-                            Donates to the following every month. </h3>  
-                        </div>
-                    </div>
-
-                    <div class="row-sm-6 container-fluid d-flex justify-content-center">
-                            <div class="col-sm-4 justify-content-center partnerImg">  
-                                <img src="assets\about us\unhcr.jpeg" class="img-fluid-partnerImage">                       
-                            </div>     
-                            
-                            <div class="col-sm-4 justify-content-center partnerImg">  
-                                <img src="assets\about us\unicef.png" class="img-fluid-partnerImage">                       
-                            </div> 
-                    </div>
+        
+        <div class="container-fluid m-0 pb-5 d-flex justify-content-center">
+            <div class="row"> 
+                <div class="align-self-center w-50 unhcr">  
+                    <img src="assets\about us\unhcr.jpeg" class="image-fluid d-flex float-end">                       
+                </div>     
+            
+                <div class="justify-content-center align-middle w-50">  
+                    <img src="assets\about us\unicef.png" class="image-fluid d-flex float-start unicef">                       
+                </div> 
+            </div>
+        </div>
 
 
-
-
-                </div> <!-- partnerships container2-->   
-            </div>  <!-- partnerships container1-->   
-        </div>  <!-- partnerships bd-->   
-    </div> <!-- pPortalTitle Container--> 
+    </div> <!-- partnerships container2-->   
+    </div>  <!-- partnerships container1-->   
+    </div>  <!-- partnerships bd-->   
+</div> <!-- pPortalTitle Container--> 
 
 
     <!-- Footer Section -->
